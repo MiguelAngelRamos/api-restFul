7dbb2448d88d725c181ff434959b3b58bee0d504
Miguel Ramos <return.infox@gmail.com>
2024-11-14 11:49:24 -0300

    entidades

--- src/main/java/com/kibernumacademy/apirest/entity/Post.java
+++ src/main/java/com/kibernumacademy/apirest/entity/Post.java
@@ -10,7 +10,40 @@ public class Post {
   private String title;
   private String content;
 
-  @ManyToOne
-  @JoinColumn(name="user_id")
+  @ManyToOne(fetch = FetchType.EAGER) // Carga al usuario asociado al Post
+  @JoinColumn(name = "user_id")
   private User user;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
 }
